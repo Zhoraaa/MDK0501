4390980bb5918ffa131e82d15c62e2631b453a4d
zh0raaa <102014678+Zhoraaa@users.noreply.github.com>
2023-03-31 02:39:32 +0500

    vscode upload

--- MDK0501/4 practice/9.php
+++ MDK0501/4 practice/9.php
@@ -1,20 +1,43 @@
 <html lang="ru">
-<?php
-    $text = $_POST['text'];
-    
-    $text_len = mb_strlen($text);
-    // Возвращаем количество слов. При втором параметре == 1 преобразует в массив со словами, при == 2 в массиве ключами будут позиции слов в строке.
-    $text_words = str_word_count($text);
-?>
 
 <form action="#" method="post">
     <textarea name="text" id="" style="width: 800px; height: 300px; padding: 5px; font-size: 16px;resize:none">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nulla odio, auctor eget vehicula sed, mattis et velit. Sed pharetra volutpat diam, nec eleifend velit pulvinar sed. Phasellus sed feugiat est, in lobortis massa. Ut eros turpis, porta sed commodo at, pulvinar pulvinar enim. Suspendisse urna sem, feugiat a mauris a, volutpat posuere diam. Aliquam id dictum mauris. Sed ante dolor, dictum iaculis est nec, pharetra laoreet eros. Suspendisse porta metus sit amet nisi suscipit, quis ultrices tellus imperdiet. Cras scelerisque ut nunc a ultricies. Cras in quam varius tellus rhoncus tincidunt.</textarea>
     <button type="submit">погна</button>
-    
 </form>
 
-<p>Количество символов: <?= $text_len ?></p>
-<p>Количество слов: <?= $text_words ?></p>
+<?php
+function poschitay($text)
+{
+    $text_len = mb_strlen($text);
+
+    // Преобразуем строку в массив, по умолчанию у функции шаг составляет 1. Можно дописать вторым аргументом.
+    $symbols = str_split($text);
+
+    // Считаем символы
+    foreach ($symbols as $symbol) {
+        if (!isset($counter[$symbol])) {
+            $counter[$symbol] = 1;
+        } elseif (isset($counter[$symbol])) {
+            $counter[$symbol]++;
+        }
+    }
+
+    foreach ($counter as $key => $item) {
+        $item .= " (" . round($item / $text_len * 100, 1) . "%)";
+        $counter[$key] = $item;
+    }
+
+    return $counter;
+}
+
+if (!empty($_POST)) {
+    $text = $_POST['text'];
+
+    echo "<pre>";
+    print_r(poschitay($text));
+    echo "</pre>";
+}
+?>
 
 <br>
 <a href="./"><button>Oбратно</button></a>
